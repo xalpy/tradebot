04768dfdd23f447a693706caa50583e3e5dd63df
xalpy <drozdivoron@gmail.com>
2025-12-12 19:07:25 +0300

    init

--- config.py
+++ config.py
@@ -3,8 +3,8 @@ TEST_API_KEY = "AEIEbMu86k978Dppw8"
 TEST_API_SECRET = "pN3PQhEE7S3YZNQUIkq2TJxndJ8X1nVE2RRl"
 
 # Реальные ключи (mainnet)
-REAL_API_KEY = "b1ZUkQTbOQ2ZBRXdr8"
-REAL_API_SECRET = "IxbXE4ABbqE5Ay4INsyQhCvLXQpyHRMA7E8k"
+REAL_API_KEY = "свой вставь"
+REAL_API_SECRET = "СВОЙ ВСТАВЬ!"
 
 # Параметры стратегии
 PIVOT_FORWARD = 125  # аналог input.int(25, ...) из Pine Script
